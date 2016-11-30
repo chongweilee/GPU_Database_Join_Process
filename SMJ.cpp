59c68a3700b0665e3160dd5a30a7849a1ec9f8d0
chongweilee <b02901088@ntu.edu.tw>
2016-11-30 23:51:57 +0800

    basic setting

---
+++ SMJ.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<fstream>
+
+using namespace std;
+
+struct data{
+    string key,val;
+}
+
+int main(){
+    ifstream rin("R.csv");
+    string tmp;
+    int n=0;
+}
